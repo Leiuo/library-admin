90051593a10c7c7db0cf6c24cee2a3952d73adf0
LEI <930452672@qq.com>
2026-06-10 11:30:02 +0800

    更改 vite.config.js 中的 base 设置

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/library-admin/',
+    base: process.env.VERCEL ? '/' : '/library-admin/',
     server: {
         port: 3000,
         open: true
